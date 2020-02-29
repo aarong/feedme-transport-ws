5a5b0ba8ce64edb809e1fb260e7766394e3c95d4
Aaron G <aarongoertzen@gmail.com>
2020-02-29 17:05:31 -0500

    Prerun update

--- tests/prerun.windows.cmd
+++ tests/prerun.windows.cmd
@@ -1,3 +1,3 @@
-echo 127.0.0.1 localhost > %temp%\temphosts.txt
+echo 127.0.0.1 testinghost.com > %temp%\temphosts.txt
 type C:\WINDOWS\system32\drivers\etc\hosts >> %temp%\temphosts.txt
 copy /Y %temp%\temphosts.txt C:\WINDOWS\system32\drivers\etc\hosts
