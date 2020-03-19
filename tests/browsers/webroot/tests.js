ff19db42953ad8e8b797e882a91f46dc10d3c8c0
Aaron G <aarongoertzen@gmail.com>
2020-03-19 07:36:29 -0400

    .

---
+++ tests/browsers/webroot/tests.js
@@ -0,0 +1,45 @@
+/*
+
+Browser tests
+
+*/
+
+// describe("Server test", function() {
+//   it("should work", function(done) {
+//     var PORT = 3000;
+//     var URL = "ws://testinghost.com:" + PORT + "/lol";
+
+//     var fmClient = feedmeClient({ transport: feedmeTransportWsClient(URL) });
+
+//     fmClient.on("connect", function() {
+//       var feed = fmClient.feed("SomeFeed", { feed: "args" });
+//       feed.on("open", function() {
+//         expect(1).toBe(1);
+//         done();
+//         console.log(feed.data());
+//         setInterval(function() {
+//           fmClient.action("SomeAction", { action: "args" }, function(
+//             err,
+//             actionData
+//           ) {
+//             console.log(err, actionData);
+//           });
+//         }, 1000);
+
+//         feed.on("action", function(an, aa, fd, ofd) {
+//           console.log(fd);
+//         });
+//       });
+
+//       feed.desireOpen();
+//     });
+
+//     fmClient.connect();
+//   });
+// });
+
+describe("tests", function() {
+  it("test", function() {
+    expect(1).toBe(1);
+  });
+});
